6764adf09330c89abdccdb9164dc77c0642829de
Jack <mariojack@live.com.au>
2024-01-25 14:12:25 +1000

    Distance check for star spawning

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    List<GameObject> stars = new List<GameObject>();
     public float minStarSpacing = 1;
     public GameObject starPrefab;
     public static GameController instance;
@@ -40,38 +41,60 @@ public class GameController : MonoBehaviour
     }
     void CreateNewGame()
     {
-        //how many stars
-        //where the ship spawns 
+        
+        
         //where are the stars
         //how many planets per star
-        //reset any player progress
+        //reset any player progress, destroy existing stars
 
         for (int i = 0; i < starsAmount; i++)
         {
 
             int safety = 1000;
             int whileCounter = 0;
+            Vector3 position = Vector3.zero;
             while (whileCounter <= safety)
             {
 
                 //Get a random position
                 //Check that that position is valid
 
+                float randomX = Random.Range(minX, maxX);
+                float randomY = Random.Range(minY, maxY);
+                position = new Vector3(randomX, randomY, -1);
+                bool positionIsValid = true;
+                for (int j = 0; j < stars.Count; j++)
+                {
+                    float dist = Vector3.Distance(position, stars[j].transform.position );
+                    Debug.Log(dist);
+                    if (dist < minStarSpacing )
+                    {
+                        positionIsValid = false;
+                        break;
+                    }
+                }
+                if (positionIsValid)
+                {
+                    break;
+                }
                 //If it is valid, break the loop  
-               // break;
+                // break;
                 whileCounter++;
             }
 
-            if (safety == whileCounter) Debug.Log("The safety was hit while spawning stars");
-
+            if (safety == whileCounter)
+            {
+                Debug.Log("The safety was hit while spawning stars");
+                break;
+            }
+            
 
             //Getting the valid position
-            float randomX = Random.Range(minX, maxX);
-            float randomY = Random.Range(minY, maxY);
-            Vector3 position = new Vector3(randomX, randomY, -1);
+            
 
             //Spawning the actual star
-            var Star = Instantiate(starPrefab, position, Quaternion.identity);
+            var star = Instantiate(starPrefab, position, Quaternion.identity);
+            stars.Add(star);
 
         } 
     }
