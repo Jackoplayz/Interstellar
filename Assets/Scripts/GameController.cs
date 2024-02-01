bada3c47a720e55bb169246b6eaf27d927441ad8
Jack <mariojack@live.com.au>
2024-02-01 13:22:10 +1000

    Cleaning up lines

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
-{
-    public GameObject headQuarters;
-    List<GameObject> stars = new List<GameObject>();
+{   
     public float minStarSpacing = 1;
     public GameObject starPrefab;
     public static GameController instance;
@@ -28,10 +26,10 @@ public class GameController : MonoBehaviour
         // Start is called before the first frame update
         void Start()
         {
-         minX = -8.3f;
-         maxX = 8.3f;
-         minY = -4.5f;
-         maxY = 4.5f;
+         minX = -8.8f;
+         maxX = 8.8f;
+         minY = -5f;
+         maxY = 5f;
         CreateNewGame();
         }
 
@@ -42,10 +40,11 @@ public class GameController : MonoBehaviour
     }
     void CreateNewGame()
     {
-        
-        
+        //how many stars
+        //where the ship spawns 
+        //where are the stars
         //how many planets per star
-        //reset any player progress, destroy existing stars
+        //reset any player progress
 
         for (int i = 0; i < starsAmount; i++)
         {
@@ -53,49 +52,29 @@ public class GameController : MonoBehaviour
             int safety = 1000;
             int whileCounter = 0;
             Vector3 position = Vector3.zero;
-            while (whileCounter < safety)
+            while (whileCounter <= safety)
             {
 
                 //Get a random position
-                
+                //Check that that position is valid
 
                 float randomX = Random.Range(minX, maxX);
                 float randomY = Random.Range(minY, maxY);
                 position = new Vector3(randomX, randomY, -1);
-               
 
-
-                float dist = Vector3.Distance(position, headQuarters.transform.position);
-                if (dist < minStarSpacing) continue;
-                //Check that that position is valid
-                bool positionIsValid = true;
-                for (int j = 0; j < stars.Count; j++)
-                {
-                     dist = Vector3.Distance(position, stars[j].transform.position );
-                   
-                    if (dist < minStarSpacing )
-                    {
-                        positionIsValid = false;
-                        break;
-                    }
-                }
-                if (positionIsValid)
-                {
-                    break;
-                }
+                //If it is valid, break the loop  
+                // break;
                 whileCounter++;
             }
 
-            //Debug.Log($"While counter: {whileCounter}");
-            if (safety == whileCounter)
-            {
-                //Debug.Log("The safety was hit while spawning stars");
-                break;
-            }
+            if (safety == whileCounter) Debug.Log("The safety was hit while spawning stars");
+
+
+            //Getting the valid position
             
+
             //Spawning the actual star
-            var star = Instantiate(starPrefab, position, Quaternion.identity);
-            stars.Add(star);
+            var Star = Instantiate(starPrefab, position, Quaternion.identity);
 
         } 
     }
